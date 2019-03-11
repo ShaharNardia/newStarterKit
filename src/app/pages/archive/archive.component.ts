162e6bf656ad33bb6bda931bd9394a23707b7991
ShaharNardia <Shahar.Nardia@gmail.com>
2019-03-11 16:09:57 +0200

    d

--- src/app/pages/archive/archive.component.ts
+++ src/app/pages/archive/archive.component.ts
@@ -26,7 +26,7 @@ export class ArchiveComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private user: UserService
-  ) {}
+  ) { }
 
   getDate(date: any) {
     const dateString = date;
@@ -34,26 +34,13 @@ export class ArchiveComponent implements OnInit {
     return newDate;
   }
 
-  fetchArticles(type: string, param1: string) {
-    if (type === 'GetArticles') {
-      this.articleService.GetArticles().subscribe(articleData => {
-        
-        this.sortedArticles = articleData;
-        console.log(this.sortedArticles);
-      });
-    } else if (type === 'GetArticlesByAuthor') {
-      this.articleService
-        .GetArticlesByAuthor(param1)
-        .subscribe(articleData => {});
-    } else if (type === 'GetArticlesByCategory') {
-      this.articleService
-        .GetArticlesByCategory(param1)
-        .subscribe(articleData => {});
-    }
-  }
+  
 
   ngOnInit() {
-    this.fetchArticles('GetArticles', '');
+    this.articleService.GetArticles().subscribe(data=>{
+      this.setParams(true,'News Letter','','0',data);
+      this.sortedArticles = data;
+    });
     this.route.queryParams.subscribe(params => {
       const Author = params['Author'];
       const Category = params['Category'];
@@ -61,61 +48,47 @@ export class ArchiveComponent implements OnInit {
       const Tag = params['Tag'];
 
       if (Author !== undefined) {
-        this.defaultSort = '0';
-        this.subscribeType = 'Author';
-        this.subscribeVal = Author;
-
-        this.fetchArticles('GetArticlesByAuthor', Author);
-
+        this.articleService.GetArticlesByAuthor(Author).subscribe(data=>{
+          this.setParams(true, 'Author', Author,'0',data);
+        });
         if (Author === this.user.userNameAuth) {
           this.showSubscribeBtn = false;
         } else {
           this.showSubscribeBtn = true;
         }
       } else if (Category !== undefined) {
-        this.setParams( true, 'Category', sessionStorage.getItem('categorySort'));
-        this.defaultSort = Category;
+        
+        
 
-        this.fetchArticles('GetArticlesByCategory', Category);
+        this.articleService.GetArticlesByCategory(Category).subscribe(data=>{
+          this.setParams(true, 'Category', sessionStorage.getItem('categorySort'),Category,data);
+        });
       } else if (SearchTerm !== undefined) {
-        this.showSubscribeBtn = true;
-        this.subscribeType = 'SearchTerm';
-        this.subscribeVal = SearchTerm;
-        this.defaultSort = 'All';
-
+        
         this.articleService
           .GetArticlesSearchResults(SearchTerm)
           .subscribe(data => {
-            console.log(data);
-            this.sortedArticles = data;
+            this.setParams(true, 'SearchTerm', SearchTerm,'0', data);
           });
         this.resultsNumberFor = `we found ${
           this.sortedArticles.length
-        } results for '${SearchTerm}'`;
+          } results for '${SearchTerm}'`;
         this.searchResLbl = `sorry, we could not find '${SearchTerm}' , please search other term...`;
         this.searchTerm = SearchTerm;
       } else if (SearchTerm === undefined) {
         this.resultsNumberFor = '';
       } else if (Tag !== undefined) {
-        this.showSubscribeBtn = true;
-        this.subscribeType = 'Tag';
-        this.subscribeVal = Tag;
-
-        this.defaultSort = 'All';
         this.articleService.GetArticlesByTags(Tag).subscribe(data => {
-          this.sortedArticles = data;
+          this.setParams(true, 'Tag', Tag, '0',data);
         });
         this.resultsNumberFor = `here ${
           this.sortedArticles.length
-        } articles with tag '${Tag}'`;
+          } articles with tag '${Tag}'`;
         this.searchResLbl = ``;
         this.searchTerm = '';
       } else {
-        this.setParams(true, 'Daily', 'NewsLetter');
-
         this.articleService.GetArticles().subscribe(data => {
-          console.log(data);
-          this.sortedArticles = data;
+          this.setParams(true, 'Daily', 'NewsLetter','',data);
         });
       }
     });
@@ -124,10 +97,16 @@ export class ArchiveComponent implements OnInit {
   setParams(
     _showSubscribeBtn: boolean,
     _subscribeType: string,
-    _subscribeVal: string
+    _subscribeVal: string,
+    _defaultSort: string,
+    _sortedArticles: any
   ) {
     this.showSubscribeBtn = _showSubscribeBtn;
     this.subscribeType = _subscribeType;
     this.subscribeVal = _subscribeVal;
+    this.sortedArticles = _sortedArticles;
+    if (_defaultSort !== '') {
+      this.defaultSort = _defaultSort;
+    }
   }
 }
